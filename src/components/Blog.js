8fbe76c01b54a5a5c459f725b4d61670444daf0f
rrgarre <rrgarre@gmail.com>
2023-05-30 20:07:54 +0200

    5-7 detalles del blog ocultables

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    <b>{blog.title}</b> by: <i>{blog.author}</i>
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+
+  const [ visible, setVisible ] = useState(false)
+  console.log('Contenido de blog: ', blog)
+  return(
+    <div className="blog">
+      {/* <p>{blog.title}</p> by: <i>{blog.author}</i> */}
+      <p>
+        {blog.title}  
+        <button onClick={()=>{setVisible(!visible)}}>
+          {
+            !visible
+              ? 'view'
+              : 'hide'
+          }
+        </button>
+      </p>
+      {
+        visible && (
+          <>
+            <p>{blog.url}</p>
+            <p>likes {blog.likes} <button>like</button></p>
+            <p>{blog.author}</p>
+          </>
+        )
+      }
+    </div>  
+  )
+}
 
 export default Blog
